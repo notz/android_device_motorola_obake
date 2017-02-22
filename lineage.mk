LineageOS/android_device_motorola_ghost

Rebrand to LineageOS
Change-Id: If72dc4069280e6304dbda1a86cedbbfc1ecc3323

lineage.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/motorola/ghost/full_ghost.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 PRODUCT_RELEASE_NAME := MOTO X
-PRODUCT_NAME := cm_ghost
+PRODUCT_NAME := lineage_ghost

notz/android_device_motorola_obake

lineage.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/motorola/obake/full_obake.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 PRODUCT_RELEASE_NAME := DROID Ultra
-PRODUCT_NAME := cm_obake
+PRODUCT_NAME := lineage_obake
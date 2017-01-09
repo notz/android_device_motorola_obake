14185829d1c2dcaa28d6326c6e256587a28f3e1f
VitaTaf <bryantan_98@hotmail.com>
2017-01-09 00:22:40 -0800

    Rebrand to LineageOS
    Change-Id: If72dc4069280e6304dbda1a86cedbbfc1ecc3323

--- lineage.mk
+++ lineage.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/motorola/obake/full_obake.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 PRODUCT_RELEASE_NAME := DROID Ultra
-PRODUCT_NAME := cm_obake
+PRODUCT_NAME := lineage_obake
